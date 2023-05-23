857be90ede37811f64364544bd547596338dfbfb
Sebastian Tremmel <littleperlmonk@gmail.com>
2023-05-24 00:39:58 +0200

    feat: projekt-uebersichts-komponente in camarilla und sabbat uebersichtsseite eingebaut

--- src/routes/camarilla/(blog)/uebersicht/+page.svelte
+++ src/routes/camarilla/(blog)/uebersicht/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { A, Carousel, Heading, P } from 'flowbite-svelte';
+	import ProjektUebersicht from '$lib/components/projektUebersicht.svelte';
+	import { getImageUrl } from 'services/directus';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -10,36 +11,15 @@
 	let images = data.bilder?.map((e) => {
 		return {
 			id: e.camarilla_uebersicht_id,
-			imgurl: `${import.meta.env.VITE_DIRECTUS_URL}/assets/${e.directus_files_id}?format=auto`
+			imgurl: getImageUrl(e.directus_files_id)
 		};
 	});
 </script>
 
-<Heading tag="h1" class="mb-4">Vampire Live - Camarilla</Heading>
-
-<div class="flex flex-col-reverse md:grid md:grid-rows-1 md:grid-cols-3 gap-4">
-	<Carousel
-		images={images ?? []}
-		{showThumbs}
-		{showCaptions}
-		{showIndicators}
-		loop
-		duration={3000}
-	/>
-	<div class="text-left col-span-2">
-		<P uppercase class="[&>p]:first-letter:text-2xl [&>p]::text-2xl [&>p]:text-justify [&>p]:mb-2">
-			{@html data.beschreibung?.code}
-		</P>
-
-		<Heading tag="h2">Spieltermine</Heading>
-		<P uppercase class="[&>p]:first-letter:text-2xl [&>p]::text-2xl [&>p]:text-justify [&>p]:mb-2">
-			{@html data.spieltermine?.code}
-		</P>
-
-		<Heading tag="h2">Kontakt</Heading>
-		<div class="grid grid-cols-min-content-first grid-rows-2 gap-x-2">
-			<P>E-Mail</P>
-			<A href={`mailto:${data.email}`}>{data.email}</A>
-		</div>
-	</div>
-</div>
+<ProjektUebersicht
+	titel="Vampire Live - Camarilla"
+	beschreibung={data.beschreibung?.code ?? ''}
+	spieltermine={data.spieltermine?.code ?? ''}
+	email={data.email ?? ''}
+	{images}
+/>
