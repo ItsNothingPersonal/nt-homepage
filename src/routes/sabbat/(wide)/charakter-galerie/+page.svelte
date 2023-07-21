cab7af612227ef74181d55b28b8f8bb78e6e7c7b
Sebastian Tremmel <littleperlmonk@gmail.com>
2023-07-21 20:27:14 +0200

    feat: filter für einzelgänger hinzugefügt
    - Button zum Filtern von Sabbat-Charaktere auf Einzelgänger hinzugefügt
    - Sabbat-Charakter-Typ angepasst und Packs als optional markiert

--- src/routes/sabbat/(wide)/charakter-galerie/+page.svelte
+++ src/routes/sabbat/(wide)/charakter-galerie/+page.svelte
@@ -3,17 +3,20 @@
 	import Indicator from '$lib/components/indicator.svelte';
 	import { isNullOrUndefined } from '$lib/util';
 	import { Button, ButtonGroup, Chevron, Dropdown, DropdownItem, Heading } from 'flowbite-svelte';
-	import { writable } from 'svelte/store';
+	import { writable, type Writable } from 'svelte/store';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 	const { packs, charaktere } = data;
 	let packFilter = writable('.*');
 	let offizierFilter = writable('');
+	let einzelgaengerFilter: Writable<boolean> = writable(false);
 
 	$: gefilterteCharaktere = charaktere.filter(
 		(c) =>
-			c.pack.name.match($packFilter) &&
+			(($einzelgaengerFilter === true && (c.pack === null || c.pack === undefined)) ||
+				($packFilter === '.*' && $einzelgaengerFilter === false) ||
+				($einzelgaengerFilter === false && c.pack?.name.match($packFilter))) &&
 			($offizierFilter.length > 0 ? !isNullOrUndefined(c.offizier) : true)
 	);
 
@@ -21,6 +24,7 @@
 		if ($packFilter.match(filter)) {
 			packFilter.set('.*');
 		} else {
+			einzelgaengerFilter.set(false);
 			packFilter.set(filter);
 		}
 	}
@@ -32,11 +36,26 @@
 			offizierFilter.set(filter);
 		}
 	}
+
+	function swapFilterEinzelgaenger() {
+		if ($einzelgaengerFilter === true) {
+			einzelgaengerFilter.set(false);
+		} else {
+			packFilter.set('.*');
+			einzelgaengerFilter.set(true);
+		}
+	}
 </script>
 
 <Heading tag="h1" class="mb-4">Charaktergalerie</Heading>
 <div class="mb-4">
 	<ButtonGroup>
+		<Button on:click={() => swapFilterEinzelgaenger()} class="relative">
+			Einzelgänger
+			{#if $einzelgaengerFilter === true}
+				<Indicator />
+			{/if}
+		</Button>
 		<Button class="relative">
 			<Chevron>Packs</Chevron>
 			{#if $packFilter !== '.*'}
