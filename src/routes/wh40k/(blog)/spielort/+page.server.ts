c09f12b4b2b45d530c468926dba936ba5d2f2ce9
Sebastian Tremmel <littleperlmonk@gmail.com>
2023-09-29 09:12:09 +0200

    fix: w40k spielort bilder laden analog zu sabbat und camarilla korrigiert

--- src/routes/wh40k/(blog)/spielort/+page.server.ts
+++ src/routes/wh40k/(blog)/spielort/+page.server.ts
@@ -1,13 +1,16 @@
+import { wh40kSpielortFiles } from '$lib/types/zod/wh40kSpielortFiles';
 import { readItems, readSingleton } from '@directus/sdk';
 import { client } from 'services/directus';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
 	const spielort = client.request(readSingleton('wh40k_spielort'));
-	const bilder = client.request(readItems('wh40k_spielort_files'));
+	const bilder = client.request(
+		readItems('wh40k_spielort_files', { filter: { directus_files_id: { _nnull: true } } })
+	);
 
 	return {
 		spielort,
-		bilder
+		bilder: wh40kSpielortFiles.array().parse(await bilder)
 	};
 }) satisfies PageServerLoad;
