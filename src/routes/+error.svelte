3947a553a3120e08aa540f6de220380771a161d0
Sebastian Tremmel <littleperlmonk@gmail.com>
2023-11-04 17:46:51 +0100

    anpassungen w40k eingebaut

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="w-full text-5xl font-extrabold text-gray-900 dark:text-white">
+	<h1>{$page.status}: {$page.error?.message}</h1>
+</div>
